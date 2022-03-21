709f4bfbd2b8edf64dff42cd2e1fabc067ef1479
ShravyaSaubhagyaArchana <100443056+ShravyaSaubhagyaArchana@users.noreply.github.com>
2022-03-21 14:53:18 +0530

    testing some examples

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
-
+// testing examples
 import java.util.List;
 
 /**
